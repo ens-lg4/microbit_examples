ca0686f64d68cc41010b8310b12d12279419bb3d
Leo Gordon <ens-lg4@users.noreply.github.com>
2020-02-11 00:15:48 +0000

    checking the passability using low-level static data structures

--- colour_lines.py
+++ colour_lines.py
@@ -4,7 +4,6 @@ from random import randint
 
 screen = neopixel.NeoPixel(pin0, 64)    # physical 8*8
 board_size  = 8                         # logical (don't have to match)
-connected   = 5
 
 
 # Enable NeoPixels to use x & y values
@@ -22,14 +21,8 @@ n_balls     = 0
 
 
 def trace_winning_coords(x, y, c):
-    'Return the list of all completed line coordinates that the current c has matched with'
-
     def trace_bidirection(dx, dy):
-        'Combine two opposite (uni)directions into one line passing through (x,y)'
-
         def trace_unidirection(dx, dy):
-            'Trace the longest line that extends from (x,y) in the given direction (dx,dy), constrained by the board edges'
-
             dist = 0
             coords = []
             nx = x + dx*(dist+1)
@@ -42,7 +35,7 @@ def trace_winning_coords(x, y, c):
             return coords
 
         bidirection_coords = trace_unidirection(dx, dy) + trace_unidirection(-dx, -dy)
-        return bidirection_coords if len(bidirection_coords) >= (connected-1) else []
+        return bidirection_coords if len(bidirection_coords)+1 >= 5 else []
 
     return (trace_bidirection(1, 0)  +
             trace_bidirection(0, 1)  +
@@ -51,7 +44,35 @@ def trace_winning_coords(x, y, c):
             [(x,y)] )
 
 
-def add_new_balls(n):   # ToDo: check that newly added balls have automatically completed some lines
+all_levels_matrix = [[0]*board_size for i in range(board_size)]
+
+def there_is_path(A, B):
+    for x in range(8):
+        for y in range(8):
+            all_levels_matrix[x][y] = 0
+
+    level = all_levels_matrix[A[0]][A[1]] = 1
+
+    while True:
+        new_edge_started = False
+        for cx in range(8):
+            for cy in range(8):
+                if all_levels_matrix[cx][cy]==level:
+                    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:   # all possible moves from C
+                        nx, ny = cx+dx, cy+dy
+                        if 0<=nx<board_size and 0<=ny<board_size:                # still within the board?
+                            if (nx,ny)==B:
+                                return True
+                            elif board[nx][ny] == 0 and all_levels_matrix[nx][ny] == 0:  # new accessible?
+                                all_levels_matrix[nx][ny] = level+1
+                                new_edge_started = True
+        if new_edge_started:
+            level += 1
+        else:
+            return False
+
+
+def add_new_balls(n):
     global n_balls
 
     for _ in range(n):
@@ -71,7 +92,8 @@ def add_new_balls(n):   # ToDo: check that newly added balls have automatically 
 add_new_balls(5)
 cursor_x = 3
 cursor_y = 3
-plot(cursor_x, cursor_y, highlight[board[cursor_x][cursor_y]])
+under_cursor = board[cursor_x][cursor_y]
+plot(cursor_x, cursor_y, highlight[under_cursor])
 selected_c = 0
 
 while True:
@@ -93,37 +115,36 @@ while True:
         under_cursor = board[cursor_x][cursor_y]
         if selected_c:
             if under_cursor==0:
-                board[cursor_x][cursor_y] = selected_c
-                board[selected_x][selected_y] = 0
-                plot(cursor_x, cursor_y, highlight[selected_c])
+                if there_is_path((selected_x, selected_y), (cursor_x, cursor_y)):
+                    board[cursor_x][cursor_y] = selected_c
+                    board[selected_x][selected_y] = 0
 
-                winning_coords = trace_winning_coords(cursor_x, cursor_y, selected_c)
-                if len(winning_coords)>1:
-                    # remove the balls at winning coords
-                    for wx, wy in winning_coords:
-                        board[wx][wy] = 0
-                        plot(wx, wy, colours[0])
+                    winning_coords = trace_winning_coords(cursor_x, cursor_y, selected_c)
+                    if len(winning_coords)>1:   # clean up the winning balls
+                        for wx, wy in winning_coords:
+                            board[wx][wy] = 0
+                            plot(wx, wy, colours[0])
+                    else:
+                        add_new_balls(3)
                 else:
-                    add_new_balls(3)
-            else:   # just unselect and revert to neutral shade
-                pass
-            selected_c = board[selected_x][selected_y]
+                    display.scroll('No way')
+            selected_c = board[selected_x][selected_y]  # unselect and revert to neutral shade
             plot(selected_x, selected_y, colours[selected_c])
             selected_c = 0
         elif under_cursor!=0:   # select a new ball
             selected_x, selected_y, selected_c = cursor_x, cursor_y, under_cursor
-            plot(selected_x, selected_y, selected[selected_c])
-
-        cursor_moved = False
+        else:
+            cursor_moved = False
         sleep(200)
     else:
         cursor_moved = False
 
     if cursor_moved:
         if selected_c and cursor_x==selected_x and cursor_y==selected_y:
-            plot(cursor_x, cursor_y, selected[board[cursor_x][cursor_y]])
+            plot(cursor_x, cursor_y, selected[under_cursor])
         else:
-            plot(cursor_x, cursor_y, colours[board[cursor_x][cursor_y]])
+            plot(cursor_x, cursor_y, colours[under_cursor])
         cursor_x, cursor_y = new_x, new_y
-        plot(cursor_x, cursor_y, highlight[board[cursor_x][cursor_y]])
+        under_cursor = board[cursor_x][cursor_y]
+        plot(cursor_x, cursor_y, highlight[under_cursor])
         sleep(200)
